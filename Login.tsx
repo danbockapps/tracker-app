e3a18cb264164f4780b66769b333615e1f69eb9f
Dan Bock <danbock@gmail.com>
2023-08-16 06:27:13 -0700

    Add login form

--- Login.tsx
+++ Login.tsx
@@ -1,18 +1,58 @@
-import { FC } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { FC, useState } from 'react'
+import { StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
 
-const Login: FC = () => (
-  <View style={styles.container}>
-    <Text>Login</Text>
-  </View>
-)
+const Login: FC = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        autoComplete='email'
+        autoCapitalize='none'
+        keyboardType='email-address'
+        style={styles.textInput}
+        placeholder='E-mail'
+        placeholderTextColor='#ccc'
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        autoComplete='password'
+        autoCapitalize='none'
+        style={styles.textInput}
+        placeholder='Password'
+        placeholderTextColor='#ccc'
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  textInput: {
+    height: 50,
+    marginHorizontal: 30,
+    marginBottom: 30,
+    borderBottomWidth: 1,
+  },
+  button: {
     alignItems: 'center',
     justifyContent: 'center',
+    height: 50,
+    marginHorizontal: 30,
+    marginBottom: 30,
+    backgroundColor: '#f099ff',
   },
 })
 
