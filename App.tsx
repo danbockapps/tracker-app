473d98813ef8a8a39af695bd25fe1c87faead942
Dan Bock <danbock@gmail.com>
2023-09-20 06:37:55 -0400

    Add some types

--- App.tsx
+++ App.tsx
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar'
 import { FC, useState } from 'react'
 import Login from './app/Login'
 import Reports from './app/Reports'
-import { AppContext } from './app/appContext'
+import { AppContext, Report } from './app/appContext'
 
 const App: FC = () => {
-  const [reports, setReports] = useState<any>()
+  const [reports, setReports] = useState<Report[]>()
 
   return (
     <AppContext.Provider value={{ reports, setReports }}>
--- app/appContext.ts
+++ app/appContext.ts
@@ -1,16 +1,18 @@
 import { createContext } from 'react'
 
-interface Report {
+export interface Report {
   week_id: number
-  weight: number
-  physact_minutes: number
-  avgsteps: number
-  notes: string
+  weight?: number
+  physact_minutes?: number
+  strength_minutes?: number
+  aerobic_minutes?: number
+  avgsteps?: number
+  notes?: string
 }
 
 interface AppContextInterface {
   reports?: Report[]
-  setReports: (x: any) => void
+  setReports: (reports: Report[]) => void
 }
 
 export const AppContext = createContext<AppContextInterface>({ setReports: () => {} })
